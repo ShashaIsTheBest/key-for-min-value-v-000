9169b93be5ac1a240a2de7ced2e7499acf52614f
Shasha Yan <shashayan@gmail.com>
2021-05-14 16:16:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,8 +7,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if lowest_value == nil || value < lowest_value
       lowest_value = v
-      lowest key = key
+      lowest_key = key
       end 
     end
-  lowest key
+  lowest_key
 end
